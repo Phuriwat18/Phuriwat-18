a838f6d9a4bf7814a712a2ccbda0b2e687febc2a
Phuriwat18 <105902493+Phuriwat18@users.noreply.github.com>
2022-09-14 10:05:55 +0700

    first commit

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.back-end.master')
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+    <div class="card">
+        <h5 class="card-header">User</h5>
+        <div class="table-responsive text-nowrap">
+          <table class="table">
+            <thead class="table-dark">
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Created_dt</th>
+                <th>Updated_dt</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody class="table-border-bottom-0">
+              @foreach ($user as $users)
+              <tr>
+                <td>{{ $users->id }}</td>
+                <td>{{ $users->name }}</td>
+                <td>{{ $users->address }}</td>
+                <td>{{ $users->email }}</td>
+                <td>{{ $users->phone }}</td>
+                <td>{{ $users->created_dt }}</td>
+                <td>{{ $users->updated_dt }}</td>
+                <td>
+                  <a href="{{ route('user.edit',$users->id) }}"><i class='bx bxs-edit'></i></a>
+                  <a href="{{ route('user.delete',$users->id) }}"><i class='bx bx-trash'></i></a>
+                </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about',function(){
+    return "หน้าเกี่ยวกับเรา";
+});
+
+Auth::routes();
+
+// profile
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//admin index
+Route::get('/admin/index',[App\Http\Controllers\HomeController::class, 'admin'])->name('admin');
+
+//user
+Route::get('/admin/user/index',[UserController::class,'index'])->name('user.index');
+Route::get('/admin/user/edit/{id}',[UserController::class,'edit'])->name('user.edit');
+Route::post('/admin/user/update/{id}',[Usercontroller::class, 'update'])->name('user.update');
+Route::get('/admin/user/delete/{id}',[Usercontroller::class, 'delete'])->name('user.delete');
+
+//category
+Route::get('/admin/category/index',[CategoryController::class,'index'])->name('category.index');
+Route::get('/admin/category/create',[CategoryController::class,'createform'])->name('category.create');
+
+//product
+Route::get('/admin/product/index',[ProductController::class,'index'])->name('product.index');
+Route::get('/admin/product/create',[ProductController::class,'createform'])->name('product.create');
+
